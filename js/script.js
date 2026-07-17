3e3df744c2dfae2987dfa1862c2a4dd3c8ee14b4
ReneTheTeacher <teacherrened@gmail.com>
2026-07-16 23:03:09 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function updateAnimations() {
     profile.style.opacity = Math.max(0, 1 - scroll / 350);
 
     // Move the text up smoothly
-    const move = Math.min(scroll * 0.15, 80);
+    const move = Math.min(scroll * 0.4, 80);
     text.style.transform = `translate3d(0, -${move}px, 0)`;
 
     ticking = false;
